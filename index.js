407a64750b1953f80c53edb9eb1a5d6b682de723
thiagotfsilva <thiagophsilva@hotmail.com>
2026-09-16 19:18:09 -0300

    iniciando index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import { App } from "@slack/bolt";
 import dotenv from "dotenv";
+import express from "express";
+import pkg from '@slack/bolt';
+
 
 dotenv.config();
+const { App } = pkg;
 
 const log = {
     info: (msg, ...args) => console.log(`[INFO] ${msg}`, ...args),
@@ -8,4 +13,14 @@ const log = {
     debug: (msg, ...args) => process.env.NODE_ENV === "development" && console.log(`[DEBUG] ${msg}`, ...args),
 }
 
-log.info("Hello, world!");
+class SlackAIAgent {
+    constructor() {
+        this.app = express();
+        this.slack = new App({
+            token: process.env.SLACK_BOT_TOKEN,
+            signingSecret: process.env.SLACK_SIGNING_SECRET,
+            socketMode: true, 
+            appToken: process.env.SLACK_APP_TOKEN,
+        });
+    }
+}
